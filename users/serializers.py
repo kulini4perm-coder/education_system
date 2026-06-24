74ca0e60167f4e94fd15d4dc6bff10d06afb507f
Artem Kulinich <kulini4perm@gmail.com>
2026-06-24 13:19:50 +0500

    start

--- users/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from users.models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'email', 'phone', 'city', 'avatar', 'first_name', 'last_name']
-        read_only_fields = ['email']
--- users/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import generics
-from users.models import User
-from users.serializers import UserSerializer
-
-class UserProfileUpdateAPIView(generics.RetrieveUpdateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
