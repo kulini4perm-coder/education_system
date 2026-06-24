3a50adfcd067f667468da63d82a4d5ef921cc227
Artem Kulinich <kulini4perm@gmail.com>
2026-06-24 13:04:09 +0500

    Initial commit with education system structure

---
+++ materials/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+class Course(models.Model):
+    title = models.CharField(
+        max_length=150,
+        verbose_name="Название курса"
+    )
+    preview = models.ImageField(
+        upload_to="materials/course_previews/",
+        blank=True,
+        null=True,
+        verbose_name="Превью (картинка)"
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание курса"
+    )
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name="lessons",
+        verbose_name="Курс"
+    )
+    title = models.CharField(
+        max_length=150,
+        verbose_name="Название урока"
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Описание урока"
+    )
+    preview = models.ImageField(
+        upload_to="materials/lesson_previews/",
+        blank=True,
+        null=True,
+        verbose_name="Превью (картинка)"
+    )
+    video_url = models.URLField(
+        blank=True,
+        null=True,
+        verbose_name="Ссылка на видео"
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self):
+        return self.title
+
