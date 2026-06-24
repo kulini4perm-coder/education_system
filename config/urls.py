cede84b04f861fed0fb05cc869952cf6c89f28be
Artem Kulinich <kulini4perm@gmail.com>
2026-06-24 13:14:23 +0500

    Initial blank project structure

--- config/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('materials.urls', namespace='materials')),
-    path('users/', include('users.urls', namespace='users')),
-]
