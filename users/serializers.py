af00f2213ad66520d89500e54324e25eb0bbcb67
Artem Kulinich <kulini4perm@gmail.com>
2026-06-26 14:32:35 +0500

    Serializers, payment model, fixture and filtering setup

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from users.models import User
+from users.models import User, Payment
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'phone', 'city', 'avatar', 'first_name', 'last_name']
         read_only_fields = ['email']
+
+class PaymentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Payment
+        fields = '__all__'
--- users/views.py
+++ users/views.py
@@ -1,8 +1,19 @@
 from rest_framework import generics
-from users.models import User
-from users.serializers import UserSerializer
+from users.models import User, Payment
+from users.serializers import UserSerializer, PaymentSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+
+
 
 class UserProfileUpdateAPIView(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class PaymentListAPIView(generics.ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+    filter_backends = [DjangoFilterBackend, OrderingFilter] # Бэкенды для фильтрации и сортировки
+    filterset_fields = ("paid_course", "paid_lesson", "payment_method") # поля для точной фильтрации
+    ordering_fields = ("payment_date",) # поля для сортировки (по дате оплаты)
